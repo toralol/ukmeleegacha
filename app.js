7c2e6e21dcc5bb913ca727538a97c7d11ce0b6b1
Nao <ninenates@gmail.com>
2026-03-17 22:44:28 +0000

    update js to account for new player count and to clear results field on reroll

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // var id = document.getElementById("playerrank")
 
 function returnChar() {
+    document.getElementById("playerName").textContent = ""
     //var id = parseInt(document.getElementById("playerId").value) - 1;
     for(let i = 0; i < 5; i++) {
-        id = Math.floor(Math.random() * 200);
+        id = Math.floor(Math.random() * 590);
         console.log(id);
         document.getElementById("playerName").append(players[id].Player + " ")
     }
