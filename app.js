8aa59103292046298d6bff5be049ed5499730781
Nao-SPARROW <ninenaos@gmail.com>
2026-03-18 07:51:26 +0000

    add newline to name display

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function returnChar() {
     for(let i = 0; i < 5; i++) {
         id = Math.floor(Math.random() * 590);
         console.log(id);
-        document.getElementById("playerName").append(players[id].Player + " ")
+        document.getElementById("playerName").append(players[id].Player + "\n")
     }
     // document.getElementById("playerName").textContent = players[id].Player
 }
